8186e2831e7dfba4ceba3ce6f82ac3ef55947157
BBFre <2931470156@qq.com>
2022-07-21 22:56:29 +0800

    bad

--- client.py
+++ client.py
@@ -69,7 +69,7 @@ def getImg(imgParam):
 if __name__ == '__main__':
     imgParam = {
         "img_url": 'https://c2cpicdw.qpic.cn/offpic_new/0/2931470156-1644525957-E9DC8C8A20E03758A1A37C5830C6625B/0?term=3',
-        "output": "\\data\\ercrftvgybhu.jpg",
+        "output": "testRemote0.jpg",
         'language': 'chinese',
         "mode": "standard",
         "background": "white",
